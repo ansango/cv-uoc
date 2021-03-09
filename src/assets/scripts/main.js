d17964f42a1e1d504685efcdaf9f22d41d0306df
Anibal Santos <anibalsantosgo@gmail.com>
2021-03-09 02:41:05 +0100

    starting

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,23 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+/**
+ * Write any other JavaScript below
+ */
+
+/**
+ * Libraries
+ */
+import "@fortawesome/fontawesome-free/css/all.css";
+/**
+ * Libraries
+ */
++(function () {
+  const university = "UOC";
+  console.log(`Hello, ${university}!`);
+})();
